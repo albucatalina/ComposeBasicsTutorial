413fbb56b74ddaaf50d70c4d283d89529fe50e67
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-03-25 23:49:37 +0200

    Business Card module

--- businesscard/src/main/java/com/example/businesscard/MainActivity.kt
+++ businesscard/src/main/java/com/example/businesscard/MainActivity.kt
@@ -4,16 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import com.example.businesscard.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    BusinessCard()
                 }
             }
         }
@@ -34,16 +52,90 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BusinessCard(modifier: Modifier = Modifier) {
+fun BusinessCard() {
     val backgroundImage = painterResource(id = R.drawable.business_card_background)
-    Box(modifier){
+    val profilePicture = painterResource(id = R.drawable.android_logo)
+
+    Box(
+        contentAlignment = Alignment.TopCenter
+    ){
         Image(
             painter = backgroundImage,
             contentDescription = null,
             contentScale = ContentScale.Crop,
-            alpha = 0.9f
+            alpha = 0.7f,
+            //modifier = Modifier.fillMaxHeight()
         )
-        
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = profilePicture,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(top = 150.dp)
+                        .background(Color.Black)
+                        .width(100.dp)
+                        .padding(15.dp)
+                )
+                Text(
+                    text = "Catalina Albu",
+                    fontSize = 36.sp,
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                )
+                Text(
+                    text = "Future Android Developer",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = Color(0xFF3DDC84)
+                )
+            }
+
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier
+                    .padding(top = 250.dp)
+            ) {
+                Row{
+                    Icon(Icons.Default.Phone, contentDescription = null)
+                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+                    Text(
+                        text = "0712 345 678",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+                Row(
+                    modifier = Modifier.padding(top = 5.dp)
+                ){
+                    Icon(Icons.Default.Share, contentDescription = null)
+                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+                    Text(
+                        text = "@AndroidDev",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+                Row(
+                    modifier = Modifier.padding(top = 5.dp)
+                ){
+                    Icon(Icons.Default.Email, contentDescription = null)
+                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+                    Text(
+                        text = "mobile@android.ro",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium
+                    )
+                }
+            }
+        }
+
     }
 
 }
