68f00c237b25f7f84e40d8d3732e113117591477
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-03-29 10:00:56 +0200

    Display dice image on the screen
    Add images resources and display dice with value 1 on the screen

--- diceroller/src/main/java/com/example/diceroller/MainActivity.kt
+++ diceroller/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +32,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    DiceRollerApp()
                 }
             }
         }
     }
 }
 
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = "1"
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
