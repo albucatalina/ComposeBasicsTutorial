c7b900b9e3c93000e69753bdadb726c8ff1582a4
Albu Catalina <87013834+albucatalina@users.noreply.github.com>
2024-04-01 08:26:54 +0300

    First screen -Lemonade tree

--- lemonade/src/main/java/com/example/lemonade/MainActivity.kt
+++ lemonade/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.lemonade
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +41,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Lemonade()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Lemonade(modifier: Modifier = Modifier){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.lemon_tree),
+            contentDescription = stringResource(id = R.string.cd_image1),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(id = R.string.screen1_text),
+            fontSize = 18.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LemonadePreview(){
+    LearnTogetherTheme {
+        Lemonade()
     }
 }
